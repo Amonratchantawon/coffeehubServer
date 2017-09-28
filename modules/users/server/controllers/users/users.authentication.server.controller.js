527e58108d6b89d0e7ab4738fbda070ce128a532
amonratCha <amonrat.cha@northbkk.ac.th>
2017-09-28 16:34:44 +0700

    hhhhhhhhhhhh

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -7,6 +7,7 @@ var path = require('path'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
   mongoose = require('mongoose'),
   passport = require('passport'),
+  Shop = mongoose.model('Shop'),
   User = mongoose.model('User');
 
 // URLs for which user can't be redirected on signin
@@ -45,6 +46,7 @@ exports.signup = function (req, res) {
         if (err) {
           res.status(400).send(err);
         } else {
+          user.populate(shop);
           res.json(user);
         }
       });
